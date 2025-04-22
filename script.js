e29e2317e227511433ed04e0bf51ba44dccc7994
SmitJadav13 <smitjadav1010@gmail.com>
2025-04-22 21:19:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+// particles.js config
+particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 60
+      },
+      "color": {
+        "value": "#00ffc3"
+      },
+      "shape": {
+        "type": "circle"
+      },
+      "opacity": {
+        "value": 0.4
+      },
+      "size": {
+        "value": 3
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#00ffc3",
+        "opacity": 0.3,
+        "width": 1
+      },
+      "move": {
+        "speed": 2
+      }
+    },
+    "interactivity": {
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"
+        }
+      }
+    }
+  });
+  
